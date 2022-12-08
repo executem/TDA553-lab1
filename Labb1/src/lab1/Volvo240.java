074d3f4a70ff4faabb894d03619ea6bab6abd330
jakobschy <117906842+jakobschy@users.noreply.github.com>
2022-12-08 13:44:26 +0100

    Update Volvo240.java

--- Labb1/src/lab1/Volvo240.java
+++ Labb1/src/lab1/Volvo240.java
@@ -15,14 +15,4 @@ public class Volvo240 extends Car{
     public double speedFactor(){
         return getEnginePower() * 0.01 * trimFactor;
     }
-        
-    @Override
-    public void incrementSpeed(double amount){
-	    setCurrentSpeed(Math.min(getCurrentSpeed() + speedFactor() * amount,getEnginePower()));
-        }
-
-    @Override
-    public void decrementSpeed(double amount){
-        setCurrentSpeed(Math.max(getCurrentSpeed() - speedFactor() * amount,0));
-    }
 }
