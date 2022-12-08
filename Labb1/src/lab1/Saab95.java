312d6f0d08148e6082c8e060031a954156021714
jakobschy <117906842+jakobschy@users.noreply.github.com>
2022-12-08 13:44:20 +0100

    Update Saab95.java

--- Labb1/src/lab1/Saab95.java
+++ Labb1/src/lab1/Saab95.java
@@ -26,13 +26,4 @@ public class Saab95 extends Car{
         if(turboOn) turbo = 1.3;
         return getEnginePower() * 0.01 * turbo;
     }
-
-    @Override
-    public void incrementSpeed(double amount){
-        setCurrentSpeed(getCurrentSpeed() + speedFactor() * amount);
-    }
-
-    public void decrementSpeed(double amount){
-        setCurrentSpeed(getCurrentSpeed() - speedFactor() * amount);
-    }
 }
